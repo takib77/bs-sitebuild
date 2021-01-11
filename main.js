1804904c82dc695a918960d62b1ac6e1b095f647
Leo <takib77@gmail.com>
2021-01-11 13:48:12 +0100

    Header and 2nd section fixed

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 'use strict';
 
+const headerStyle = () => {
+    const header = document.querySelector('.sticky-top');
+    if (window.scrollY < 10) header.className = 'sticky-top justify-content-center p-0 bg-transparent';
+    else header.className = 'sticky-top justify-content-center p-0 bg-white';
+};
+
+const brandStyle = () => {
+    const navbarBrand = document.querySelector('.navbar-brand')
+    if (window.scrollY < 10) navbarBrand.className = 'navbar-brand text-warning';
+    else navbarBrand.className = 'navbar-brand text-dark';
+};
+
+const linkStyle = () => {
+    const navLink = document.querySelectorAll('.nav-link')
+    if (window.scrollY < 10) {
+        navLink[0].className = 'nav-link text-white';
+        navLink[1].className = 'nav-link text-white';
+        navLink[2].className = 'nav-link text-white';
+    }
+    else {
+        navLink[0].className = 'nav-link text-dark';
+        navLink[1].className = 'nav-link text-dark';
+        navLink[2].className = 'nav-link text-dark';
+    }
+};
+
 window.onscroll = () => {
-    const nav = document.querySelector('.sticky-top');
-    if (window.scrollY < 10) nav.className = 'navbar navbar-expand-lg navbar-light bg-transparent sticky-top';
-    else nav.className = 'navbar navbar-expand-lg navbar-light bg-white sticky-top';
+    headerStyle();
+    brandStyle();
+    linkStyle();
 };
